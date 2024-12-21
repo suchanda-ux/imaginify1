5cd002726c13e419a213d5c787c1d7234308fdb7
Suchanda Kundu <suchanda88@gmail.com>
2024-12-22 05:02:46 +1100

    initial-commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,5 @@
-"use server";
-
+"use server"
 import { revalidatePath } from "next/cache";
-
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
